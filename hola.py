6ead74266578afabf680671e962519c457f71c92
glodnn <109389574+glodnn@users.noreply.github.com>
2023-04-11 21:16:02 +0100

    dda

--- hola.py
+++ hola.py
@@ -1,6 +1,3 @@
 print("Hello World")
-print("Hello")asv ydvuyatw dyawv dawyud hawiybd uwaydb awd+
-awd 
-awdvawd bawawdaçdawdaw
-d awìujdb aw awdvawdd
-awd awd a
+print("Hello")
+print("moskaMarikon69")
