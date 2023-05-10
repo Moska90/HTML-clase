5a02cfd0981c6c65dcfab030cd26bb567eaec95a
Moska90 <hugomaso06@gmail.com>
2023-05-10 16:34:19 +0200

    asg
    Signed-off-by: Moska90 <hugomaso06@gmail.com>

--- JS/imagen.js
+++ JS/imagen.js
@@ -4,26 +4,22 @@ function splitImage() {
 
     var newDiv = document.createElement('div');
     newDiv.classList.add('image1');
-
-    // Agregar evento de clic al div
     newDiv.addEventListener('click', function() {
-    window.location.href = 'atacantes.html'; // Reemplaza 'ruta/pagina.html' con la ruta de la página a la que deseas redirigir
+    window.location.href = 'atacantes.html';
     });
     image.parentNode.appendChild(newDiv);
 
     var newDiv = document.createElement('div');
     newDiv.classList.add('image2');
-
-    // Agregar evento de clic al div
     newDiv.addEventListener('click', function() {
-    window.location.href = 'defensores.html'; // Reemplaza 'ruta/pagina.html' con la ruta de la página a la que deseas redirigir
+    window.location.href = 'defensores.html';
     });
+
     image.parentNode.appendChild(newDiv);
     image.remove();
 
     setTimeout(function() {
       newImage.style.width = '350px';
       newImage1.style.width = '350px';
-      image.remove();
     }, 400);
   }
