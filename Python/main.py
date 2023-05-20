3ac0f074be70f994461ad8bb2b647aae5cab491c
Moska90 <hugomaso06@gmail.com>
2023-05-20 19:34:39 +0200

    RIBERA TONTO
    Signed-off-by: Moska90 <hugomaso06@gmail.com>

--- Python/main.py
+++
@@ -1,5 +0,0 @@
-print(["Ribera puta" for _ in range(10)])
-
-# <py-script src="Python/main.py"></py-script>
-# <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css"/>
-# <script defer src="https://pyscript.net/latest/pyscript.js"></script> 
